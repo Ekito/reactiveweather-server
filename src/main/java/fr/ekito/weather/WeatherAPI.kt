248ba55cb743880b51d969ddf6bce1f01de33e1d
Arnaud Giuliani <agiuliani@ekito.fr>
2017-09-26 17:22:18 +0200

    statis version

--- src/main/java/fr/ekito/weather/WeatherAPI.kt
+++ src/main/java/fr/ekito/weather/WeatherAPI.kt
@@ -1,23 +1,15 @@
 package fr.ekito.weather
 
-import fr.ekito.weather.google.GoogleMapsGeocodeWS
-import fr.ekito.weather.google.json.geocode.Geocode
-import fr.ekito.weather.wunderground.WundergroundWS
-import fr.ekito.weather.wunderground.json.weather.Weather
 import org.slf4j.LoggerFactory
-import org.springframework.cache.CacheManager
-import org.springframework.cache.annotation.Cacheable
 import org.springframework.http.HttpStatus
-import org.springframework.http.ResponseEntity
-import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.web.bind.annotation.*
-import java.io.IOException
 
 /**
  * Created by arnaud on 01/08/2016.
  */
 @RestController
-class WeatherAPI(val apiKey: ApiKey, val wundergroundWS: WundergroundWS, val googleMapsGeocodeWS: GoogleMapsGeocodeWS, val cacheManager: CacheManager) {
+//class WeatherAPI(val apiKey: ApiKey, val googleMapsGeocodeWS: GoogleMapsGeocodeWS, val cacheManager: CacheManager) {
+class WeatherAPI() {
 
     // logger
     var logger = LoggerFactory.getLogger(WeatherAPI::class.java)
@@ -27,35 +19,41 @@ class WeatherAPI(val apiKey: ApiKey, val wundergroundWS: WundergroundWS, val goo
     @ResponseStatus(HttpStatus.OK)
     fun hello() = "hello :)"
 
-    @Scheduled(fixedRate = 3600000)
-    fun evictWeather() {
-        logger.warn("clear cache")
-        cacheManager.getCache("weathers").clear()
-    }
+//    @Scheduled(fixedRate = 3600000)
+//    fun evictWeather() {
+//        logger.warn("clear cache")
+//        cacheManager.getCache("weathers").clear()
+//    }
 
-    @Cacheable("weathers")
+    //    @Cacheable("weathers")
     @RequestMapping(path = arrayOf("/weather"), method = arrayOf(RequestMethod.GET))
-    fun weather(@RequestParam("lat") lat: String, @RequestParam("lon") lon: String, @RequestParam("lang") lang: String): ResponseEntity<Weather> {
-        try {
-            val response = wundergroundWS.weather(apiKey.wunderground, lang.toUpperCase(), lat, lon).execute()
-            return ResponseEntity(response.body(), HttpStatus.valueOf(response.code()))
-        } catch (e: IOException) {
-            e.printStackTrace()
-            return ResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR)
-        }
+    fun weather(@RequestParam("lat") lat: String, @RequestParam("lon") lon: String, @RequestParam("lang") lang: String): String = WeatherApp.weather
+//
+//            : ResponseEntity<Weather> {
+////        try {
+////            val response = wundergroundWS.weather(apiKey.wunderground, lang.toUpperCase(), lat, lon).execute()
+////            return ResponseEntity(response.body(), HttpStatus.valueOf(response.code()))
+////        } catch (e: IOException) {
+////            e.printStackTrace()
+////            return ResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR)
+////        }
+//        return ResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR)
+//    }
 
-    }
-
-    @Cacheable("geocodes")
+    //    @Cacheable("geocodes")
     @RequestMapping(path = arrayOf("/geocode"), method = arrayOf(RequestMethod.GET))
-    fun geocode(@RequestParam("address") address: String): ResponseEntity<Geocode> {
-        try {
-            val response = googleMapsGeocodeWS!!.weather(address, apiKey!!.geocode).execute()
-            return ResponseEntity(response.body(), HttpStatus.valueOf(response.code()))
-        } catch (e: IOException) {
-            e.printStackTrace()
-            return ResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR)
-        }
-    }
+    fun geocode(@RequestParam("address") address: String): String = WeatherApp.geocode
+
+
+//            : ResponseEntity<Geocode> {
+////        return try {
+////            val response = googleMapsGeocodeWS.weather(address, apiKey.geocode).execute()
+////            ResponseEntity(response.body(), HttpStatus.valueOf(response.code()))
+////        } catch (e: IOException) {
+////            e.printStackTrace()
+////            ResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR)
+////        }
+//        return ResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR)
+//}
 
 }
--- src/main/java/fr/ekito/weather/WeatherApp.kt
+++ src/main/java/fr/ekito/weather/WeatherApp.kt
@@ -1,18 +1,13 @@
 package fr.ekito.weather
 
-import fr.ekito.weather.google.GoogleMapsGeocodeWS
-import fr.ekito.weather.wunderground.WundergroundWS
-import okhttp3.OkHttpClient
-import okhttp3.logging.HttpLoggingInterceptor
-import org.slf4j.LoggerFactory
-import org.springframework.beans.factory.annotation.Value
+import org.springframework.beans.factory.InitializingBean
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.cache.annotation.EnableCaching
-import org.springframework.context.annotation.Bean
+import org.springframework.context.ApplicationContext
+import org.springframework.core.io.Resource
 import org.springframework.scheduling.annotation.EnableScheduling
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
 
 /**
  * Created by arnaud on 01/08/2016.
@@ -20,36 +15,51 @@ import retrofit2.converter.gson.GsonConverterFactory
 @SpringBootApplication
 @EnableCaching
 @EnableScheduling
-class WeatherApp {
+class WeatherApp : InitializingBean {
+
+    @Autowired
+    lateinit var ctx: ApplicationContext
+
+    override fun afterPropertiesSet() {
+        geocode = readJson(ctx.getResource("classpath:json/geocode_toulouse.json"))
+        weather = readJson(ctx.getResource("classpath:json/weather_toulouse.json"))
+    }
+
+    fun readJson(fileRsr: Resource): String = fileRsr.inputStream.bufferedReader().useLines { sequence -> sequence.joinToString("\n") }
 
     // logger
-    val logger = LoggerFactory.getLogger(WeatherApp::class.java)
+//    val logger = LoggerFactory.getLogger(WeatherApp::class.java)
 
-    @Bean
-    fun wundergroundWSClient(): WundergroundWS {
-        val retrofit = Retrofit.Builder()
-                .baseUrl("http://api.wunderground.com/api/")
-                .client(createClient())
-                .addConverterFactory(GsonConverterFactory.create()).build()
-        return retrofit.create(WundergroundWS::class.java)
-    }
+//    @Bean
+//    fun wundergroundWSClient(): WundergroundWS {
+//        val retrofit = Retrofit.Builder()
+//                .baseUrl("http://api.wunderground.com/api/")
+//                .client(createClient())
+//                .addConverterFactory(GsonConverterFactory.create()).build()
+//        return retrofit.create(WundergroundWS::class.java)
+//    }
 
-    private fun createClient(): OkHttpClient = OkHttpClient.Builder().addInterceptor(HttpLoggingInterceptor()).build()
+//    private fun createClient(): OkHttpClient = OkHttpClient.Builder().addInterceptor(HttpLoggingInterceptor()).build()
 
-    @Bean
-    fun googleMapsGeocodeWS(): GoogleMapsGeocodeWS {
-        val retrofit = Retrofit.Builder()
-                .baseUrl("https://maps.googleapis.com/maps/api/geocode/")
-                .client(createClient())
-                .addConverterFactory(GsonConverterFactory.create()).build()
-        return retrofit.create(GoogleMapsGeocodeWS::class.java)
-    }
+//    @Bean
+//    fun googleMapsGeocodeWS(): GoogleMapsGeocodeWS {
+//        val retrofit = Retrofit.Builder()
+//                .baseUrl("https://maps.googleapis.com/maps/api/geocode/")
+//                .client(createClient())
+//                .addConverterFactory(GsonConverterFactory.create()).build()
+//        return retrofit.create(GoogleMapsGeocodeWS::class.java)
+//    }
 
-    @Bean
-    fun apiKey(@Value("\${WUNDERGROUND_KEY}") wundergroundKey: String, @Value("\${GEOCODE_KEY}") googleGeocodeApiKey: String): ApiKey {
-        logger.warn(" wunderground key : " + wundergroundKey)
-        logger.warn(" geocode key : " + googleGeocodeApiKey)
-        return ApiKey(wundergroundKey, googleGeocodeApiKey)
+//    @Bean
+//    fun apiKey(@Value("\${WUNDERGROUND_KEY}") wundergroundKey: String, @Value("\${GEOCODE_KEY}") googleGeocodeApiKey: String): ApiKey {
+//        logger.warn(" wunderground key : " + wundergroundKey)
+//        logger.warn(" geocode key : " + googleGeocodeApiKey)
+//        return ApiKey(wundergroundKey, googleGeocodeApiKey)
+//    }
+
+    companion object {
+        lateinit var geocode: String
+        lateinit var weather: String
     }
 }
 
